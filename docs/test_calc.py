361d90bddd380fa6fd69c6145893a7fb4819337d
NotAnotherError <99984505+NotAnotherError@users.noreply.github.com>
2023-07-24 08:32:49 -0400

    renamed calculator buttons

---
+++ docs/test_calc.py
@@ -0,0 +1,26 @@
+import math
+
+#information gathering
+    induct_rate = input(float("What is the induct rate?"))
+    goal_volume = input(float("What is the goal volume?"))
+    current_time = input(float("What is the current time?"))
+    end_time = input(float("What is the end time?"))
+
+#calculator 1 
+
+rate_per_min = induct_rate / 60
+possible_volume = rate_per_min * time_remaining
+
+#time remaining (the difference between current time and end time in minutes)
+
+final_volume = current_volume + possible goal_volume
+
+#compare final_volume to goal_volume if final_volume is greater than goal_volume print final_volume in green, if it is less than goal_volume print final_volume in red
+
+#Calculator 2
+
+goal_volume - current volume = remaining_volume
+x_volume = remaining_volume / time remaining 
+rate_per_hour = x_volume * 60
+
+# compare induct rate to rate_per_hour if rate_per_hour is greater than induct rate print it in yellow, if it is equal print it in green, if it is less than print it in red
